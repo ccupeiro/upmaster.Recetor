d1684cf9b90d1cd0c9ec0b7593fe926511711e32
Carlos Casa <carlos.cupeiro@grupocmc.es>
2016-11-30 22:14:57 +0100

    Initial commit

---
+++ app/src/main/java/com/upvmaster/carlos/recetor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.upvmaster.carlos.recetor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
