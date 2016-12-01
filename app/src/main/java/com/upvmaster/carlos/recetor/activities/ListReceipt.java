d4b8256e67e9ada4171c8c9f65ebe3e10e264b59
Carlos Casa <carlos.cupeiro@grupocmc.es>
2016-12-01 21:42:27 +0100

    Ventana Inicial completa, comienzo de la lista de recetas

---
+++ app/src/main/java/com/upvmaster/carlos/recetor/activities/ListReceipt.java
@@ -0,0 +1,15 @@
+package com.upvmaster.carlos.recetor.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.upvmaster.carlos.recetor.R;
+
+public class ListReceipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_receipt);
+    }
+}
---
+++ app/src/main/java/com/upvmaster/carlos/recetor/activities/MainActivity.java
@@ -0,0 +1,53 @@
+package com.upvmaster.carlos.recetor.activities;
+
+import android.animation.ObjectAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.upvmaster.carlos.recetor.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_recetas,btn_random,btn_dietas;
+    private boolean animado=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img_logo = (ImageView) findViewById(R.id.img_logo);
+        btn_recetas = (Button) findViewById(R.id.btn_recetas);
+        btn_random = (Button) findViewById(R.id.btn_recetas);
+        btn_dietas = (Button) findViewById(R.id.btn_recetas);
+        LinearLayout grupo_btn = (LinearLayout) findViewById(R.id.grupo_botones);
+        Animation anim_logo = AnimationUtils.loadAnimation(this,R.anim.anim_logo);
+        Animation anim_botones = AnimationUtils.loadAnimation(this,R.anim.anim_botones);
+        if(!animado){
+            animado = true;
+            img_logo.startAnimation(anim_logo);
+            grupo_btn.startAnimation(anim_botones);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
