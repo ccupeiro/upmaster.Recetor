508d3b6f49fb4d8cb80976d92bbcf56a96025e46
Carlos Cupeiro <carlos.cupeiro@grupocmc.es>
2016-12-07 09:09:36 +0100

    Subida que faltaba

--- app/src/main/java/com/upvmaster/carlos/recetor/activities/AddReceipt_Activity.java
+++ app/src/main/java/com/upvmaster/carlos/recetor/activities/AddReceipt_Activity.java
@@ -1,8 +1,50 @@
 package com.upvmaster.carlos.recetor.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.upvmaster.carlos.recetor.R;
+import com.upvmaster.carlos.recetor.utils.UtilsReceipt;
+
 /**
  * Created by carlos.cupeiro on 07/12/2016.
  */
 
-public class AddReceipt_Activity {
+public class AddReceipt_Activity extends AppCompatActivity {
+
+    TableLayout tl_ingredientes;
+    TableLayout tl_pasos;
+    TableLayout tl_variante;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_receta);
+        tl_ingredientes = (TableLayout) findViewById(R.id.tbl_ingredientes);
+        tl_pasos = (TableLayout) findViewById(R.id.tbl_pasos);
+        tl_variante = (TableLayout) findViewById(R.id.tbl_variantes);
+        LinearLayout ll_add = (LinearLayout) findViewById(R.id.ll_add_ingrediente);
+        ll_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addIngrediente(null);
+            }
+        });
+    }
+
+    private void addIngrediente(View vista){
+        TableRow ingredienteRow = new TableRow(this);
+        ingredienteRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        View ingredienteRowInterno = getLayoutInflater().inflate(R.layout.elemento_add_ingrediente, null,false);
+        //insertarlo en la tabla
+        ingredienteRow.addView(ingredienteRowInterno);
+        tl_ingredientes.addView(ingredienteRow);
+    }
 }
--- app/src/main/java/com/upvmaster/carlos/recetor/activities/ListReceipt_Activity.java
+++ app/src/main/java/com/upvmaster/carlos/recetor/activities/ListReceipt_Activity.java
@@ -1,9 +1,12 @@
 package com.upvmaster.carlos.recetor.activities;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.upvmaster.carlos.recetor.R;
 import com.upvmaster.carlos.recetor.adapters.ViewTabAdapter;
@@ -17,6 +20,13 @@ public class ListReceipt_Activity extends AppCompatActivity {
         /*if(getSupportActionBar()!=null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
 
+        FloatingActionButton fab_add = (FloatingActionButton) findViewById(R.id.fab_add);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarAdd(null);
+            }
+        });
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.text_tab_Alph)));
         tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.text_tab_Group)));
@@ -42,5 +52,11 @@ public class ListReceipt_Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    private void lanzarAdd(View view){
+        Intent intent = new Intent(this,AddReceipt_Activity.class);
+        startActivity(intent);
     }
 }
